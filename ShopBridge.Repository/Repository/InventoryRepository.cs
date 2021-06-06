590783f5086de6fbbe9e949746b45af54cdcc93c
athulnp <athulonline13@gmail.com>
2021-06-06 12:13:56 +0530

    Removed packages

--- ShopBridge.Repository/Repository/InventoryRepository.cs
+++ ShopBridge.Repository/Repository/InventoryRepository.cs
@@ -1,12 +1,10 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Options;
 using ShopBridge.Models.Models;
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopBridge.Repository
